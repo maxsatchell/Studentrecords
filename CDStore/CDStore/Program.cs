564e5885b01cff9258185677de055a33dedf60c9
Maximilian Satchell <14ict772@stowe.co.uk>
2017-12-12 11:46:56 +0000

    End of the lesson

--- CDStore/CDStore/Program.cs
+++ CDStore/CDStore/Program.cs
@@ -7,11 +7,12 @@ namespace CDStore
     {
         static void Main(string[] args)
         {
+
             var context = new CDStoreDbContext();
             bool keepGoing = true;
             while (keepGoing)
             {
-                Console.Write("\n\nEnter 1 to add an Artist \n2 to List Artists \n3 Find artist\n9 to Quit : ");
+                Console.Write("\n\nEnter 1 to add an Artist \n2 to List Artists \n3 Find artist\n4 Find CD\n5 Add a Song\n6 Add a new CD\n9 to Quit : ");
                 char key = Console.ReadLine()[0];
                 switch (key)
                 {
@@ -23,6 +24,15 @@ namespace CDStore
                         break;
                     case '3':
                         FindArtist(context);
+                        break;
+                    case '4':
+                        FindCDByTitle(context);
+                        break;
+                    case '5':
+                        AddSong(context);
+                        break;
+                    case '6':
+                        AddCD(context);
                         break;
                     case '9':
                         keepGoing = false;
@@ -36,6 +46,11 @@ namespace CDStore
             Console.WriteLine("Enter Artist's name: ");
             var name = Console.ReadLine();
             var artist = context.Artists.FirstOrDefault(a => a.Name.Contains(name));
+            foreach (Song s in artist.Songs)
+            {
+                Console.WriteLine(s.Title + " " + s.MusicType);
+            }
+
             Console.WriteLine("Artist: " + artist.Name);
         }
 
@@ -55,6 +70,42 @@ namespace CDStore
             Artist a = new Artist() { Name = name };
             Console.WriteLine("Saving ...");
             context.Artists.Add(a);
+            context.SaveChanges();
+        }
+
+        private static void FindCDByTitle(CDStoreDbContext context)
+        {
+            Console.WriteLine("Enter the name of the song title: ");
+            string title = Console.ReadLine();
+            CD cd = context.CDs.FirstOrDefault(a => a.Title.Contains(title));
+            foreach (Song s in cd.Songs)
+            {
+                Console.WriteLine(s.Title + " " + s.Artist.Name);
+            }
+
+        }
+        private static void AddSong(CDStoreDbContext context)
+        {
+            Console.WriteLine("Enter name of new song: ");
+            string name = Console.ReadLine();
+            Song a = new Song() { Title = name };
+            Console.WriteLine("Saving ...");
+            context.Songs.Add(a);
+            context.SaveChanges();
+        }
+        private static void AddCD(CDStoreDbContext context)
+        {
+            Console.WriteLine("Enter name of new CD: ");
+            string name = Console.ReadLine();
+            CD cd = new CD() { Title = name, Published = DateTime.Today };
+            Console.WriteLine("Saving ...");
+            Console.WriteLine("Adding songs to your CD");
+            context.CDs.Add(cd);
+            context.SaveChanges();
+            Console.WriteLine("Enter the name of a song that you wish to add: ");
+            string title = Console.ReadLine();
+            Song song1 = context.Songs.FirstOrDefault(a => a.Title.Contains(title));
+            cd.Songs.Add(song1);
             context.SaveChanges();
         }
     }
